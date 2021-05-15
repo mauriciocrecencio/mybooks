6f32db0f5bfb1f6fbca7d183d9d6fd9817212128
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-05-15 18:05:43 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import GlobalStyles from './styles/global'
 
 function App () {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+    <GlobalStyles/>
+    </>
   )
 }
 
