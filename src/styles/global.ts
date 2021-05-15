ca3d3dbf106e496dff50a628f5588d334008917f
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-05-15 18:15:25 -0300

    add global background to body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
+import backgroundImage from '../assets/images/background-image.jpg'
 
 export default createGlobalStyle`
+  body {
+    background-position: center;
+    background-image: url(${backgroundImage});
+    height: 100vh;
+  }
   * {
     margin: 0;
     padding: 0;
