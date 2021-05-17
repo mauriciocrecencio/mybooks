b11a0912d9e0d2f9d572013acd6b364231cfee29
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2021-05-17 03:58:42 -0300

    adjust title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -4,8 +4,8 @@ import * as S from './styles'
 export default function Title() {
   return (
     <S.Container>
-      <S.MainTitle>bem vindo ao MYBOOKS!</S.MainTitle>
-      <S.SubTitle>pesquise por livros e faça uma lista dos seus favoritos :)</S.SubTitle>
+      <S.MainTitle>MYBOOKS</S.MainTitle>
+      <S.SubTitle>Pesquise por livros e faça uma lista dos seus favoritos!</S.SubTitle>
     </S.Container>
   )
 }
